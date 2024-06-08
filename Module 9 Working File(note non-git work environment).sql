51112a8879ce98717703611121395cd3908b740f
wallago-caite <caite.greenwood@gmail.com>
2024-06-08 18:11:30 -0500

    created QDBD in postgresql instead of sql, and imported files

--- Module 9 Working File(note non-git work environment).sql
+++ Module 9 Working File(note non-git work environment).sql
@@ -1,12 +1,34 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+---CREATE TABLES WITH COLUMNS ASSOCIATED using QUICKDBD
+---IMPORT THE DATA FROM CSVS AFTER CREATED(manually)
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+
+drop table if exists employees
+drop table if exists dept_emp
+drop table if exists departments
+drop table if exists dept_manager
+drop table if exists salaries
+drop table if exists titles
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" char(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(40)   NOT NULL,
+    "last_name" varchar(40)   NOT NULL,
+    "sex" char(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
     "dept_no" char(4)   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no","dept_no"
+        "emp_no"
      )
 );
 
@@ -22,19 +44,6 @@ CREATE TABLE "dept_manager" (
     "dept_no" char(4)   NOT NULL,
     "emp_no" int   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" char(5)   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(40)   NOT NULL,
-    "last_name" varchar(40)   NOT NULL,
-    "sex" char(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
@@ -55,15 +64,22 @@ CREATE TABLE "titles" (
      )
 );
 
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
 
---
+++ QuickDBD-export (2).sql
@@ -0,0 +1,92 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [employees] (
+    [emp_no] int  NOT NULL ,
+    [emp_title_id] char(5)  NOT NULL ,
+    [birth_date] date  NOT NULL ,
+    [first_name] varchar(40)  NOT NULL ,
+    [last_name] varchar(40)  NOT NULL ,
+    [sex] char(1)  NOT NULL ,
+    [hire_date] date  NOT NULL ,
+    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [dept_emp] (
+    [emp_no] int  NOT NULL ,
+    [dept_no] char(4)  NOT NULL ,
+    CONSTRAINT [PK_dept_emp] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [departments] (
+    [dept_no] char(4)  NOT NULL ,
+    [dept_name] varchar(40)  NOT NULL ,
+    CONSTRAINT [PK_departments] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
+    )
+)
+
+CREATE TABLE [dept_manager] (
+    [dept_no] char(4)  NOT NULL ,
+    [emp_no] int  NOT NULL ,
+    CONSTRAINT [PK_dept_manager] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [salaries] (
+    [emp_no] int  NOT NULL ,
+    [salary] int  NOT NULL ,
+    CONSTRAINT [PK_salaries] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
+
+CREATE TABLE [titles] (
+    [title_id] char(5)  NOT NULL ,
+    [title] varchar(40)  NOT NULL ,
+    CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
+        [title_id] ASC
+    )
+)
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [titles] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_emp_no]
+
+ALTER TABLE [salaries] WITH CHECK ADD CONSTRAINT [FK_salaries_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [salaries] CHECK CONSTRAINT [FK_salaries_emp_no]
+
+COMMIT TRANSACTION QUICKDBD
